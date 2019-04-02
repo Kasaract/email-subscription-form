ab7bff2fa2b7b049dfad53fc32a0c28f78f5c035
Kasaract <35552077+Kasaract@users.noreply.github.com>
2019-04-02 14:42:02 -0400

    Updates

---
+++ precache-manifest.4e60434f2937772ca0e8b7cf292b00f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef1184afa5806698ae61",
+    "url": "/email-subscription-form/static/js/runtime~main.2a21828c.js"
+  },
+  {
+    "revision": "d99cc3eb79153d7aab92",
+    "url": "/email-subscription-form/static/js/main.1df37818.chunk.js"
+  },
+  {
+    "revision": "8d52be860827c43ab76a",
+    "url": "/email-subscription-form/static/js/2.27abd3f7.chunk.js"
+  },
+  {
+    "revision": "d99cc3eb79153d7aab92",
+    "url": "/email-subscription-form/static/css/main.2e066398.chunk.css"
+  },
+  {
+    "revision": "e1c6afb1b7d8f84a57f21692dc353586",
+    "url": "/email-subscription-form/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.87e0391691d5984f7f318873b8764e7a.js"
+  "/email-subscription-form/precache-manifest.4e60434f2937772ca0e8b7cf292b00f3.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/email-subscription-form/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
