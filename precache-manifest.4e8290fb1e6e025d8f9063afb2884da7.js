67b2394c7c15444acf16edf5a98c36dc60822623
Kasaract <35552077+Kasaract@users.noreply.github.com>
2019-04-02 02:02:41 -0400

    Updates

---
+++ precache-manifest.4e8290fb1e6e025d8f9063afb2884da7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d225420977fda0dfe043",
+    "url": "/subscription-form/static/js/runtime~main.8d70c32f.js"
+  },
+  {
+    "revision": "808457f4d77a5ca9ff30",
+    "url": "/subscription-form/static/js/main.e93da12b.chunk.js"
+  },
+  {
+    "revision": "8d52be860827c43ab76a",
+    "url": "/subscription-form/static/js/2.27abd3f7.chunk.js"
+  },
+  {
+    "revision": "808457f4d77a5ca9ff30",
+    "url": "/subscription-form/static/css/main.300ea29b.chunk.css"
+  },
+  {
+    "revision": "bb022f894d287b130ce3b6d3b46a4f9e",
+    "url": "/subscription-form/index.html"
+  }
+];
